740dcebeef4d7f36c15db022daec7233d77902d9
mayankr1307 <mayankr20027@gmail.com>
2024-08-01 20:41:50 +0530

    Commit 3, Added content in IntroActivity.kt

--- app/src/main/java/project/fitfusion/activities/IntroActivity.kt
+++ app/src/main/java/project/fitfusion/activities/IntroActivity.kt
@@ -2,11 +2,27 @@ package project.fitfusion.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import project.fitfusion.R
+import project.fitfusion.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    private var binding: ActivityIntroBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.flSignUp?.setOnClickListener {
+
+        }
+        binding?.flSignIn?.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
     }
 }
