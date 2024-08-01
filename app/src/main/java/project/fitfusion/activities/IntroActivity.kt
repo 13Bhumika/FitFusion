906da3a1bbe1c6c68e5f495bc9305300a4182630
mayankr1307 <mayankr20027@gmail.com>
2024-08-01 19:20:23 +0530

    Commit 2, Added IntroActivity.kt

---
+++ app/src/main/java/project/fitfusion/activities/IntroActivity.kt
@@ -0,0 +1,12 @@
+package project.fitfusion.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import project.fitfusion.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
---
+++ app/src/main/java/project/fitfusion/activities/SplashActivity.kt
@@ -0,0 +1,29 @@
+package project.fitfusion.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import project.fitfusion.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        moveToIntroActivity()
+    }
+
+    private fun moveToIntroActivity() {
+        val timer = object : CountDownTimer(3000, 3000) {
+            override fun onTick(p0: Long) {}
+
+            override fun onFinish() {
+                startActivity(Intent(this@SplashActivity, IntroActivity::class.java))
+                finish()
+            }
+        }
+
+        timer.start()
+    }
+}
